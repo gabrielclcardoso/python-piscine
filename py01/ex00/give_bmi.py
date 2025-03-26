5a74b71a91f644b566ae2b543c16cb2a761b6070
gabrielclcardoso <gabrielclcardoso@gmail.com>
2025-03-26 20:54:18 -0300

    feat: apply limit finished

--- py01/ex00/give_bmi.py
+++ py01/ex00/give_bmi.py
@@ -1,3 +1,16 @@
+def give_bmi(height: list[int | float],
+             weight: list[int | float]) -> list[int | float]:
+    """Given lists of height and size of the same length, the function will
+    return a list with the BMI index for each pair. If the lists have different
+    sizes or are not comprised of int or float the function will print an error
+    and return an empty list
+    """
+
+    if not lists_are_valid(height, weight) or len(height) == 0:
+        return []
+    return [(w / (h ** 2)) for h, w in zip(height, weight)]
+
+
 def lists_are_valid(l1, l2) -> bool:
     """Checks if the lists can be processed by the give_bmi function"""
 
@@ -16,21 +29,30 @@ def lists_are_valid(l1, l2) -> bool:
     return True
 
 
-def give_bmi(height: list[int | float],
-             weight: list[int | float]) -> list[int | float]:
-    """Given lists of height and size of the same length, the function will
-    return a list with the BMI index for each pair. If the lists have different
-    sizes or are not comprised of int or float the function will print an error
-    and return an empty list
+def apply_limit(bmi: list[int | float], limit: int) -> list[bool]:
+    """Given lists of BMI indices and a limit, the function will return a list
+    of booleans with True for those above the limit. If an error occurs or the
+    list is empy it returns an empty list
     """
 
-    if not lists_are_valid(height, weight) or len(height) == 0:
+    if not list_is_valid(bmi):
         return []
-    return [(w / (h ** 2)) for h, w in zip(height, weight)]
+    else:
+        return [i > limit for i in bmi]
 
 
-def apply_limit(bmi: list[int | float], limit: int) -> list[bool]:
-    pass
+def list_is_valid(lst: list[int | float]) -> bool:
+    """Given a list, checks if it is processable by the apply_limit function"""
+
+    if not isinstance(lst, list):
+        print("Error: Arguments is not a list")
+        return False
+    elif len(lst) == 0:
+        return False
+    elif not isinstance(lst[0], int | float):
+        print("Error: list does not contain float or int")
+        return False
+    return True
 
 
 def main():
