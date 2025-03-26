90b666f62656e42aa1271c0a06af67d0cd88e496
gabrielclcardoso <gabrielclcardoso@gmail.com>
2025-03-26 12:14:36 -0300

    fix: handles non iterable exception on filter.py

--- ex06/ft_filter.py
+++ ex06/ft_filter.py
@@ -1,3 +1,6 @@
+from collections.abc import Iterable
+
+
 # def main():
 #     l1 = list[1, 2, 3, 4, 5]
 #     l2 = list[0, 1, 0, 3, 0, 5, 0, 7, 0, 9]
@@ -8,7 +11,7 @@
 #     is_same_sequence(s1, s2)
 #
 #     s1 = filter(None, l2)
-#     s2 = ft_filter(None, l2)
+#     s2 = ft_filter(None, 2)
 #     is_same_sequence(s1, s2)
 #
 #     s1 = filter(str.isalpha, string)
@@ -25,6 +28,9 @@
 
 def ft_filter(function, iterable):
     """Recreation of the built in function filter"""
+    if not isinstance(iterable, Iterable):
+        print("Error: did not receive iterable")
+        return
     if function:
         sequence = [i for i in iterable if function(i)]
     else:
