f6eefcc07bf664afd247cfb6a8708c35f9d216c6
gabrielclcardoso <gabrielclcardoso@gmail.com>
2025-03-27 10:59:17 -0300

    feat: add more tests to main

--- py01/ex00/give_bmi.py
+++ py01/ex00/give_bmi.py
@@ -55,13 +55,22 @@ def list_is_valid(lst: list[int | float]) -> bool:
     return True
 
 
-def main():
-    height = [2.71, 1.15]
-    weight = [165.3, 38.4]
-    bmi = give_bmi(height, weight)
-    print(bmi, type(bmi))
-    print(apply_limit(bmi, 26))
-
-
-if __name__ == "__main__":
-    main()
+# def main():
+#     height = [2.71, 1.15]
+#     weight = [165.3, 38.4]
+#     fake = ['a', 'b']
+#
+#     bmi = give_bmi(height, weight)
+#     print(bmi, type(bmi))
+#     bmi = give_bmi([], [])
+#     print(bmi, type(bmi))
+#
+#     bmi = give_bmi(fake, height)
+#     bmi = give_bmi(None, height)
+#     bmi = give_bmi(height, None)
+#
+#     print(apply_limit(bmi, 26))
+#
+#
+# if __name__ == "__main__":
+#     main()
