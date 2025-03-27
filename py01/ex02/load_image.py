9fda4fc4db77b8b2d74dc68b5339089d2541131a
gabrielclcardoso <gabrielclcardoso@gmail.com>
2025-03-27 14:44:10 -0300

    feat: finished ex02

---
+++ py01/ex02/load_image.py
@@ -0,0 +1,28 @@
+from PIL import Image
+import numpy as np
+import sys
+
+
+def ft_load(path: str) -> np.ndarray:
+    try:
+        image = Image.open(path)
+    except Exception as e:
+        print(f"Error: {e}", file=sys.stderr)
+        return None
+
+    arr = np.array(image)
+    print(f"The shape of image is: {arr.shape}")
+    np.set_printoptions(threshold=1)
+    return arr
+
+
+# def main():
+#     print(ft_load("landscape.jpg"))
+#     print(ft_load("animal.jpeg"))
+#
+#     print(ft_load("non-existant.jpeg"))
+#     print(ft_load("load_image.py"))
+#
+#
+# if __name__ == "__main__":
+#     main()
