d7b26f9cd29eea1cd20a8685e667031c238cdf65
gabrielclcardoso <gabrielclcardoso@gmail.com>
2025-03-28 21:45:16 -0300

    feat: ex00 done

---
+++ py02/ex00/load_csv.py
@@ -0,0 +1,24 @@
+import pandas as pd
+
+
+def load(path: str) -> pd.DataFrame:
+    """Loads csv file and prints its shape and content.
+    in case of an error returns None
+    """
+
+    try:
+        data = pd.read_csv(path)
+    except Exception as e:
+        print(f"Error: {e}")
+        return None
+
+    print(f"Loading dataset of dimenstions {data.shape}")
+    print(data)
+
+
+# def main():
+#     print(load("life_expectancy_years.csv"))
+#
+#
+# if __name__ == "__main__":
+#     main()
