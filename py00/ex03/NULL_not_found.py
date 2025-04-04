fbc82267b33b48039488f4991fad25c07a4c9862
gabrielclcardoso <gabrielclcardoso@gmail.com>
2025-04-04 12:58:49 -0300

    feat: add main tester

--- py00/ex03/NULL_not_found.py
+++ py00/ex03/NULL_not_found.py
@@ -20,3 +20,21 @@ def NULL_not_found(object: any) -> int:
     else:
         print('Type not Found')
         return 1
+
+
+# def main():
+#     Nothing = None
+#     Garlic = float("NaN")
+#     Zero = 0
+#     Empty = ""
+#     Fake = False
+#     NULL_not_found(Nothing)
+#     NULL_not_found(Garlic)
+#     NULL_not_found(Zero)
+#     NULL_not_found(Empty)
+#     NULL_not_found(Fake)
+#     print(NULL_not_found("Brian"))
+#
+#
+# if __name__ == "__main__":
+#     main()
